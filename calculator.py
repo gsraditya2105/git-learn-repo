ebc6bc9805cc4af57fcc0051de76563ca4e88927
Aditya Gsr <gsraditya2105@gmail.com>
2023-02-08 15:06:28 +0530

    added calculator app code

---
+++ calculator.py
@@ -0,0 +1,20 @@
+def add(x,y):
+    print(x+y)
+    
+def sub(x,y):
+    print(x-y)
+
+def mul(x,y):
+    print(x*y)
+    
+def div(x,y):
+    print(x/y)
+
+
+if __name__ == "__main__":
+    x = 10
+    y = 20
+    add(x,y)
+    sub(x,y)
+    mul(x,y)
+    div(x,y)
