f4ab16a3d66fc140afea0df379a888904cca62d3
Aditya Gsr <gsraditya2105@gmail.com>
2023-02-08 13:56:28 +0530

    added timestamp with greet message

--- demo.py
+++ demo.py
@@ -1,7 +1,9 @@
 import datetime 
 
 def greet():
-    print("Hey Hi...Welcome to the demo")
+    print(datetime.datetime.now(),"::Hey Hi...Welcome to the demo")
+    
+
 
 if __name__ == "__main__":
     greet()
